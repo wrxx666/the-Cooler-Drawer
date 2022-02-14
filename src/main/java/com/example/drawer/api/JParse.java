cd8fe33f87824d2e067de52d331fcfca8e8d1dff
Lapaev <maxf74@live.com>
2022-02-14 13:10:31 +0500

    ...

--- src/main/java/com/example/drawer/api/JParse.java
+++ src/main/java/com/example/drawer/api/JParse.java
@@ -3,9 +3,10 @@ package com.example.drawer.api;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.BufferedReader;
 import java.io.IOException;
 
 /**
@@ -19,17 +20,18 @@ public class JParse {
         String token = (String) answerOwner.get("token");
 
         //HashMap<String,String> getJson = new HashMap<>();
+        //Gson
         answerOwner.remove("token");
         answerOwner.put("Authorization",token);
 
         TalkerGet.getRest(token);//TODO Добавить некий хранитель токена, чтобы можно было делать не только гет запросы
 
-        FileWriter fw = new FileWriter("./src/main/java/com/example/drawer/api/UserData.json");   //Записывает в жесон
+        FileWriter fw = new FileWriter("./src/main/java/com/example/drawer/api/Token.json");   //Записывает в жесон
         fw.write(answerOwner.toJSONString());
         fw.close();
 
 
-        FileReader reader = new FileReader("./src/main/java/com/example/drawer/api/UserData.json"); //читает из жесона
+        FileReader reader = new FileReader("./src/main/java/com/example/drawer/api/Token.json"); //читает из жесона
         BufferedReader br = new BufferedReader(reader);
         String buffer = "";
         while(br.ready()){
